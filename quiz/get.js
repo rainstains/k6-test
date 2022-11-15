779fac73e95dfdef9c6a43e32b03dfa7ed58c56c
rainstains <biladt.hakim@gmail.com>
2022-11-15 16:04:33 +0700

    update get & post using describe

--- quiz/get.js
+++ quiz/get.js
@@ -1,22 +1,29 @@
 import http from 'k6/http'
-import { expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.0/index.js';
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.0/index.js';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    vus: 10,
-    duration: '3s'
+    stages: [
+        { duration: '2s', target: 10 },
+        { duration: '4s', target: 20 }, // stay at 100 users for 10 minutes
+        { duration: '1s', target: 10 }, // ramp-down to 0 users    
+      ],
 }
 
-export default function() {
-    const idUser = randomIntBetween(1,12)
-    let response = http.get('https://reqres.in/api/users/'+idUser)
-    const res = response.json()
+export default function testSuite() {
+    describe('Get User by Random ID', () => {
+        const idUser = randomIntBetween(1,12)
+        let response = http.get('https://reqres.in/api/users/'+idUser)
+        const res = response.json()
 
-    expect(response.status, "Response Code").to.equal(200)
-    expect(response, "Valid Response").to.have.validJsonBody()
-    expect(res['data']['id'], "Id User").to.equal(idUser)
-    expect(res['data']['email'], "E-mail").to.not.be.empty
-    expect(res['data']['first_name'], "First Name").to.not.be.empty
-    expect(res['data']['last_name'], "Last Name").to.not.be.empty
+        expect(response.status, "Response Code").to.equal(200)
+        expect(response, "Valid Response").to.have.validJsonBody()
+        expect(res['data']['id'], "Id User").to.equal(idUser)
+        expect(res['data']['email'], "E-mail").to.not.be.empty
+        expect(res['data']['first_name'], "First Name").to.not.be.empty
+        expect(res['data']['last_name'], "Last Name").to.not.be.empty
+    })
 
 }
+
--- quiz/post.js
+++ quiz/post.js
@@ -1,32 +1,36 @@
 import http from 'k6/http'
-import { expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.0/index.js';
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.0/index.js';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-    vus: 10,
-    duration: '3s'
+    stages: [
+        { duration: '2s', target: 10 },
+        { duration: '4s', target: 20 }, // stay at 100 users for 10 minutes
+        { duration: '2s', target: 10 }, // ramp-down to 0 users    
+      ],
 }
 
-export default function() {    
-    var nameUser = randomString(7)
-    var jobUser = randomString(8)
+export default function testSuite() {    
+    describe('Create User', () => {
+        var nameUser = randomString(7)
+        var jobUser = randomString(8)
 
-    const url = 'https://reqres.in/api/users'
-    const body = JSON.stringify({
-        name: nameUser,
-        job: jobUser,
-    })
-    const headers = {
-        headers: {
-            'Content-Type': 'application/json',
+        const url = 'https://reqres.in/api/users'
+        const body = JSON.stringify({
+            name: nameUser,
+            job: jobUser,
+        })
+        const headers = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
         }
-    }
-    const response = http.post(url, body, headers)
-    const res = response.json()
+        const response = http.post(url, body, headers)
+        const res = response.json()
 
-    expect(response.status, "Response Code").to.eql(201)
-    expect(response, "Valid Response").to.have.validJsonBody()
-    expect(res['name'], "Name").to.eql(nameUser)
-    expect(res['job'], "Job").to.eql(jobUser)
-
+        expect(response.status, "Response Code").to.eql(201)
+        expect(response, "Valid Response").to.have.validJsonBody()
+        expect(res['name'], "Name").to.eql(nameUser)
+        expect(res['job'], "Job").to.eql(jobUser)
+    })
 }
